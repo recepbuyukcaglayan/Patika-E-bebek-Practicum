994da23ef2d6233e6fcfbb589dcc7416b96684af
recepbuyukcaglayan <101063302+recepbuyukcaglayan@users.noreply.github.com>
2022-10-18 11:03:23 +0300

    Add files via upload

---
+++ Patika/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        IEmployee employee = new Employee("Recep", 18000, 61, 2010);
+        System.out.println(employee.toString());
+    }
+}
